d80a03a57e103ad66a68431c8807be0170a30e9e
hayzemke <hay.zemke@gmail.com>
2023-02-15 15:21:10 -0500

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+// document.querySelectorAll("button")[0].addEventListener("click", function () {
+//     alert("I got clicked!!");
+// });
+
+var drumList = document.querySelectorAll(".drum");
+for (var i = 0; i < drumList.length; i++) {
+  drumList[i].addEventListener("click", function () {
+    
+})};
